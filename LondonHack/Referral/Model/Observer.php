74fd7156150bc93af1fe00b92afe979e3bbe8089
Piotr Gryko London <piotr.gryko@gmail.com>
2014-07-12 20:23:05 +0100

    Added basic module structure
    as outlined in
    http://www.magentocommerce.com/knowledge-base/entry/magento-for-dev-part
    -2-the-magento-config

--- LondonHack/Referral/Model/Observer.php
+++ LondonHack/Referral/Model/Observer.php
@@ -1,8 +1,33 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: peter
- * Date: 12/07/2014
- * Time: 19:17
- * To change this template use File | Settings | File Templates.
- */
+class LondonHack_Referral_Model_Observer {
+    const FLAG_SHOW_CONFIG = 'showConfig';
+    const FLAG_SHOW_CONFIG_FORMAT = 'showConfigFormat';
+
+    private $request;
+
+    public function checkForConfigRequest($observer) {
+        $this->request = $observer->getEvent()->getData('front')->getRequest();
+        if($this->request->{self::FLAG_SHOW_CONFIG} === 'true'){
+            $this->setHeader();
+            $this->outputConfig();
+        }
+    }
+
+    private function setHeader() {
+        $format = isset($this->request->{self::FLAG_SHOW_CONFIG_FORMAT}) ?
+            $this->request->{self::FLAG_SHOW_CONFIG_FORMAT} : 'xml';
+        switch($format){
+            case 'text':
+                header("Content-Type: text/plain");
+                break;
+            default:
+                header("Content-Type: text/xml");
+        }
+    }
+
+    private function outputConfig() {
+        die(Mage::app()->getConfig()->getNode()->asXML());
+    }
+}
+?>
+
